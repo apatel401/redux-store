dfa0aa9f9daddc13a89a5691b41ef996cbfafe32
apatel401 <51169932+apatel401@users.noreply.github.com>
2022-10-12 00:26:08 -0400

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
+  const cart = useSelector(state => state.cart)
   const isLoogedIn = useSelector(state => state.auth.isLoogedIn)
+  useEffect(() => {
+    fetch('https://redux-45ac4-default-rtdb.firebaseio.com/cartItem.json', {
+      method: "PUT",
+      body: JSON.stringify(cart)
+    })
+  
+  }, [cart])
+  
   return (
     <div className="App">
       {/* <Auth /> */}
