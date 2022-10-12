0848fd20720e6d0e11a63473b50916bc414eaff1
apatel401 <51169932+apatel401@users.noreply.github.com>
2022-10-12 01:15:36 -0400

    notification setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import React, {useEffect} from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-
+import Notification from "./components/Notification";
+import { uiActions } from "./store/ui-slice";
+let isFirstRender = true;
 function App() {
-  const cart = useSelector(state => state.cart)
-  const isLoogedIn = useSelector(state => state.auth.isLoogedIn)
+  const cart = useSelector((state) => state.cart);
+  const isLoogedIn = useSelector((state) => state.auth.isLoogedIn);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+
   useEffect(() => {
-    fetch('https://redux-45ac4-default-rtdb.firebaseio.com/cartItem.json', {
-      method: "PUT",
-      body: JSON.stringify(cart)
-    })
-  
-  }, [cart])
-  
+    if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }
+    const sendRequest = async () => {
+      //sending state as sending request
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending Request",
+          type: "warning",
+        })
+      );
+      const res = await fetch(
+        "https://redux-45ac4-default-rtdb.firebaseio.com/cartItem.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sent Request to database",
+          type: "success",
+        })
+      );
+    };
+    sendRequest().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending Request",
+          type: "error",
+        })
+      );
+    });
+  }, [cart]);
+
   return (
     <div className="App">
       {/* <Auth /> */}
+      {notification && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
       {isLoogedIn ? <Layout /> : <Auth />}
       {/* <Layout /> */}
     </div>
